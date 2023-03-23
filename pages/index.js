17e295d505a06b2e0cacdb6ba5efe6151e883a4e
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-03-22 23:21:06 -0700

    20:22

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,22 @@ export default function Home() {
         purpose={'RENT A HOME'}
         title="Rental Homes for"
         title2="Everyone"
-        
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
         />
-      <Banner 
-        purpose={'For Rent'}/>
+              <Banner
+        purpose={'BUY A HOME'}
+        title="Find, Buy & Own Your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://drive.google.com/drive/folders/1VmohYaB3PHVPrRQ20Ql44HKJaaKhLiEN?usp=sharing"
+        />
 
     </div>
   )
